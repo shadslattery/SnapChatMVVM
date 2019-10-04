7cf5b08a7848f6633c8a3e97efa373f58ba74336
shadslattery <shadslattery@gmail.com>
2019-10-03 23:32:16 -0400

    I have MainActivity, Snapadapter, SnapStory working with no red underlines

---
+++ app/src/main/java/constants/Constants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Constants {
+    public static final String BASE_URL = "http://shibe.online";
+    public static final String SHIBE_PATH = "/api/shibes";
+}
---
+++ app/src/main/java/snapchatadapter/Snapadapter.java
@@ -0,0 +1,51 @@
+package snapchatadapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.snapchatmvvm.R;
+import com.example.snapchatmvvm.model.SnapStory;
+
+import java.util.List;
+
+public class Snapadapter extends RecyclerView.Adapter<Snapadapter.SnapViewHolder> {
+
+    private List<SnapStory> pictureList;
+
+    public Snapadapter(List<SnapStory>pictureList){this.pictureList=pictureList;}
+
+@NonNull
+@Override
+    public SnapViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.snap_story_item,
+                parent,
+                false);
+
+        return new SnapViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Snapadapter.SnapViewHolder holder, int position) {
+        SnapStory friends2 = pictureList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pictureList.size();
+    }
+
+    class SnapViewHolder extends RecyclerView.ViewHolder {
+        TextView tvname;
+
+        public SnapViewHolder(@NonNull View itemView){
+            super(itemView);
+        }
+    }
+}
